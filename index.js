3e9fcf68478aa0ad2ea4dde90f3f48d6124a1f05
Sabid <sajjadsabid0@gmail.com>
2026-01-03 16:48:37 +0600

    connected to the MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqmwh22.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('digital_platform_db');
+    const parcelsCollection = db.collection('parcels');
+
+    app.get('/parcels', async(req, res) => {
+      const query = {}
+      const {email} = req.query;
+      if(email){
+        query.SenderEmail = email;
+      }
+
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post ('/parcels', async(req, res) => {
+      const parcel = req.body;
+      parcel.createAt = new Date()
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("hello world")
