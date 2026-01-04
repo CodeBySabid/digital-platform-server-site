9766ee89016896478afc7afa8748ee3baf9f920f
Sabid <sajjadsabid0@gmail.com>
2026-01-04 17:01:34 +0600

    checkout session implementation

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqmwh22.mongodb.net/?appName=Cluster0`;
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -47,18 +49,47 @@ async function run() {
       return res.send(result);
     })
 
-    app.post('/parcels', async (req, res) => {
-      const parcel = req.body;
-      parcel.createAt = new Date()
-      const result = await parcelsCollection.insertOne(parcel);
-      res.send(result)
-    })
-
     app.delete('/parcels/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await parcelsCollection.deleteOne(query);
       res.send(result);
+    })
+
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: 'USD',
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              }
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: 'payment',
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        success_url: `${process.env.YOUR_DOMAIN}?dashboard/payment-success`,
+        cancel_url: `${process.env.YOUR_DOMAIN}?dashboard/payment-cancelled`,
+      });
+      console.log(session);
+      res.send({ url: session.url })
+    });
+
+    app.post('/parcels', async (req, res) => {
+      const parcel = req.body;
+      parcel.createAt = new Date()
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
